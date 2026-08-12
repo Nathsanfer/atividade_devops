cb1cfd52a9fb39d9b6112dddcd0cead0463a3bd8
NathSanFer <158168917+Nathsanfer@users.noreply.github.com>
2026-08-11 22:29:50 -0300

    Código Java para a primeira pipeline da aula de devops

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Olá, DevOps!");
+        System.out.println("Pipeline executado com sucesso!");
+    }
+}   
